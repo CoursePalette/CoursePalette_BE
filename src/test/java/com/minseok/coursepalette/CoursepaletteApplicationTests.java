0734496492b3dd83e8eeca52024afbb26fb1e61b
99andrew99 <whalstjr199927@gmail.com>
2025-03-28 14:56:36 +0900

    Initial commit

---
+++ src/test/java/com/minseok/coursepalette/CoursepaletteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minseok.coursepalette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursepaletteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
